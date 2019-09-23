ed02e1fbfa82ef7c392153fcd0cea0563b841fa7
lbr <903529487@qq.com>
2019-09-23 17:30:15 +0800

    1

--- config/plugin.js
+++ config/plugin.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/** @type Egg.EggPlugin */
-// module.exports = {
-//   // had enabled by egg
-//   // static: {
-//   //   enable: true,
-//   // }
-// };
 exports.validate = {
   enable: true,
   package: 'egg-validate',
